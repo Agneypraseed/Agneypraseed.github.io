319b40289ee5eab737b1c9880c69166c124060d2
Agney Praseed <agneysince2000@gmail.com>
2024-03-12 17:26:24 +0530

    Avatar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
 import React from "react";
-import Typist from "react-typist";
-import { useState } from "react";
-
-const translations = {
-    hindi: "मैं जॉन हूँ",
-    spanish: "Soy Juan",
-};
-
-const handleTranslation = (lang) => {
-    setText(translations[lang]);
-};
+import Typed from "typed.js";
+import myAvatar from "../src/assets/avatar.jpg";
+import { useState, useEffect, useRef } from "react";
 
 const App = () => {
-    const [text, setText] = useState("I am John");
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ["AGNEY", "अग्नेय", "АГНЕЙ", "അഗ്നേയ"],
+            startDelay: 300,
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 100,
+            loop: true,
+            cursorChar: "",
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
 
     return (
-        <div>
-            <h4>HELLO WORLD!</h4>
-            👋 Hello. I'm AGNEY
-            <div>
-                <Typist
-                    avgTypingDelay={100}
-                    onTypingDone={() => {
-                        setTimeout(() => {
-                            handleTranslation("hindi");
-                        }, 1000); // Change the delay according to your needs
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", height: "100vh" }}>
+            <div style={{ flex: "0 0 auto", padding: "0 20px" }}>
+                <div style={{ padding: 250, textAlign: "left" }}>
+                    <h4>HELLO WORLD! 👋</h4>
+                    I'm{" "}
+                    <strong style={{ color: "lightpink" }}>
+                        <span ref={el} style={{ fontSize: "34px" }}></span>
+                    </strong>
+                </div>
+            </div>
+            <div style={{ flex: "0 0 auto", marginRight: "100px" }}>
+                <img
+                    src={myAvatar}
+                    alt="home pic"
+                    style={{
+                        width: "200px",
+                        height: "200px",
+                        borderRadius: "50%",
+                        padding: "20px",
                     }}
-                >
-                    {text}
-                </Typist>
+                />
             </div>
         </div>
     );
