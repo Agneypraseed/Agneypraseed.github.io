6dc718d1ca8fb596f03cd049ad9f8377cd6b194a
Agneypraseed <48130024+Agneypraseed@users.noreply.github.com>
2026-09-11 22:02:13 +0000

    Deploying to gh-pages from @ Agneypraseed/Agneypraseed.github.io@935aaf57d778338625c9e637ecc645febfa1ef9a 🚀

---
+++ assets/sample-essay-IITBcnv1.js
@@ -0,0 +1,4 @@
+import{u as s,j as n}from"./index-C8IV-fHi.js";function o(e){const t={blockquote:"blockquote",h1:"h1",p:"p",...s(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(t.h1,{children:"Hello world."}),`
+`,n.jsxs(t.blockquote,{children:[`
+`,n.jsx(t.p,{children:'“A small beginning is still a beginning"'}),`
+`]})]})}function l(e={}){const{wrapper:t}={...s(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{l as default};
